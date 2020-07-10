6c79d52fc40feed8316e5de3fd25533b46e06eb3
John Robling <roblingjohn@gmail.com>
2020-07-09 20:59:25 -0400

    allowed selection of role from add employee query

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 let departmentsArray = [];
+let rolesArray = []
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -109,7 +110,7 @@ function addRole(){
     ]).then(function(res){
         const deptObject = data.filter(object => object.name === res.newRoleDept);
         console.log(deptObject)
-        connection.query(`INSERT INTO roles (title, salary, department_id) VALUES ("${res.newRoleName}", "${res.newRoleSalary}", "${deptObject[0].id}");`, function(err, results) {
+        connection.query("INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)", [res.newRoleName, res.newRoleSalary, deptObject[0].id], function(err, results) {
             if (err) throw err;
         console.log(`Role added: ${res}`)
         askFunction();
@@ -119,21 +120,39 @@ function addRole(){
 }
 
 function addEmployee(){
+    connection.query("SELECT * FROM roles", (err, data) => {
+        rolesArray = data.map((object) => object.title)
+    console.log(rolesArray)
     inquirer.prompt([
         {
             type: "input",
-            message: "Please enter the name of the employee you would like to enter:",
-            name: "newEmployeeName"
+            message: "Please enter the first name of the employee you would like to enter:",
+            name: "firstName"
+        },
+        {
+            type: "input",
+            message: "Please enter the last name of the employee you would like to enter:",
+            name: "lastName"
+        },
+        {
+            type: "list",
+            message: "Please choose the role to which this employee belongs:",
+            choices: rolesArray,
+            name: "newRoleDept"
         }
     ]).then(function(res){
-        connection.query(`INSERT INTO roles (name) VALUES ("${res.newEmployeeName}");`, function(err, results) {
+        const roleObject = data.filter(object => object.name === res.newEmployeeRole);
+        console.log(roleObject)
+        connection.query("INSERT INTO employees (first_name, last_name, role_id, department_id, manager_id) VALUES (?, ?, ?, ?, ?)", [res.firstName, res.lastName, roleObject[0].id, roleObject[0].department_id, 0], function(err, results) {
             if (err) throw err;
             console.log("Employee added.")
         askFunction();
         })
     })
+})
 }
 
 function exit(){
     connection.end();
 }
+
